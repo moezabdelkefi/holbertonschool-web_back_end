a0162c1371d26e0a1d181c5ff80493bd2392fd51
moezabdelkef <moezbadelkefi17@gmail.com>
2023-09-20 11:49:43 +0100

    Basic annotations - concat

--- python_variable_annotations/0-main.py
+++ python_variable_annotations/0-main.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+
+"""
+ a type-annotated function add that takes a float
+ a and a float b as arguments and returns their sum as a float
+"""
 add = __import__('0-add').add
 
 print(add(1.11, 2.22) == 1.11 + 2.22)
