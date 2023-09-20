69fe3135553a13addb39374a758d3a687e17f045
moezabdelkef <moezbadelkefi17@gmail.com>
2023-09-20 12:21:22 +0100

    Basic annotations - to string

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -8,7 +8,7 @@ a float n as argument and returns the string representation of the float
 
 def to_str(n: float) -> str:
     """
-     takes a float n as argument and returns the string 
+     takes a float n as argument and returns the string
      representation of the float
 
      args:
