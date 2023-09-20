08d61cd5a00ae23af59598f01098441bb342d904
moezabdelkef <moezbadelkefi17@gmail.com>
2023-09-20 12:02:15 +0100

    Basic annotations - concatt

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -9,11 +9,11 @@
 def add(a: float, b: float) -> float:
     """
     Calculate the sum of two float numbers.
-    
+
     Args:
         a (float): The first input float.
         b (float): The second input float.
-        
+
     Returns:
         float: The sum of a and b as a float.
     """
