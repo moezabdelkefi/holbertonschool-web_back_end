c364b66a7dae6c12b31a38c2874ff6654bd561a4
moezabdelkef <moezbadelkefi17@gmail.com>
2023-09-20 12:02:46 +0100

    Basic annotations - concatt

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -9,11 +9,11 @@ and a string str2 as arguments and returns a concatenated string
 def concat(str1: str, str2: str) -> str:
     """
     Calculate the sum of two strings.
-    
+
     Args:
         str1 (str): The first input str.
         str2 (str): The second input str.
-        
+
     Returns:
         str: The sum of str1 and str2 as a string.
     """
