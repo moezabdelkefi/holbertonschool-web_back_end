2181f351e889de076b8aa59c6215cf284fbc0be7
moezabdelkef <moezbadelkefi17@gmail.com>
2023-09-23 13:35:54 +0100

    Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 import asyncio
 import random
+from typing import Generator
 
 """
  a coroutine called async_generator that takes no arguments
 """
 
 
-async def async_generator():
+async def async_generator() -> Generator:
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.randint(0, 10)
