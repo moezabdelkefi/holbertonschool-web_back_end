12a356d63d429079b6e65dd855f28a74eaa1f79f
moezabdelkef <moezbadelkefi17@gmail.com>
2023-09-25 14:40:47 +0100

    FIFO caching

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""""""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # Find the first item that was put in the cache (FIFO)
+            first_key = next(iter(self.cache_data))
+            print(f"DISCARD: {first_key}")
+            del self.cache_data[first_key]
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None:
+            return None
+
+        return self.cache_data.get(key)
