26cb9d77e8c500f79d0f51bdcf9298cf2ab27f9c
moezabdelkef <moezbadelkefi17@gmail.com>
2023-09-25 14:27:09 +0100

    Basic dictionary

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,11 @@
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is not None:
+            return self.cache_data.get(key)
