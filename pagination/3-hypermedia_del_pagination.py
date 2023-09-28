9b8d5e0e5cef9f2377ad93eddf67161d8c8a29ab
moezabdelkef <moezbadelkefi17@gmail.com>
2023-09-28 23:38:49 +0100

    Deletion-resilient hypermedia pagination

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -25,6 +25,9 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Retrieve a page of data from the indexed dataset with hypermedia 
+        information.
+        """
         assert isinstance(
             index, int) and index >= 0
         assert isinstance(
