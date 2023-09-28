7c2f9e4ff8a0c03f41b49b71ce99d166652f8738
moezabdelkef <moezbadelkefi17@gmail.com>
2023-09-28 23:02:20 +0100

    Simple helper function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,7 +3,18 @@
 Simple helper function
 """
 
+
 def index_range(page, page_size):
+    """
+    Calculate the start and end indices for a given page and page size.
+
+    Args:
+    page (int): The page number (1-indexed).
+    page_size (int): The number of items per page.
+
+    Returns:
+    tuple: A tuple containing the start index (inclusive) and end index (exclusive).
+    """
     if page < 1 or page_size <= 0:
         return None
 
