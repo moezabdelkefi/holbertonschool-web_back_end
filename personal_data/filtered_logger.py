8f419da36cf1b841a2245aff3df0748cccba560b
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-02 19:00:10 +0100

    Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,10 +7,9 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    pattern = '|'.join(map(re.escape, fields))
-    filtered_message = re.sub(pattern, redaction, message)
-
-    if filtered_message != message:
-        return filtered_message
-    else:
-        return False
+    parts = message.split(separator)
+    for i, part in enumerate(parts):
+        key_value = part.split('=')
+        if key_value[0] in fields:
+            parts[i] = f"{key_value[0]}={redaction}"
+    return separator.join(parts)
