3a61e02a4e1a3bec503fb8ebadaa86874183352d
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-02 18:54:58 +0100

    Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -11,6 +11,6 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     filtered_message = re.sub(pattern, redaction, message)
 
     if filtered_message != message:
-        return re.sub(pattern, redaction, message)
+        return filtered_message
     else:
         return False
