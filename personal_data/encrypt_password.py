5a2ac15b1b27a55c080a7b014d8e4f04ae35c11f
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-05 23:59:48 +0100

    Check valid password

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+hashing and validating passwords using the bcrypt
+hashing algorithm
+"""
+import bcrypt
+
+
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+
+def is_valid(hashed_password, password):
+    try:
+        hashed_password_str = hashed_password.decode('utf-8')
+
+        return bcrypt.checkpw(password.encode('utf-8'),
+                              hashed_password_str.encode('utf-8'))
+    except (UnicodeDecodeError, ValueError):
+        return False
