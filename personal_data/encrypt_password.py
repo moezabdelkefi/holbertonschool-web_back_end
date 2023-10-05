12afc79210d4535a6559d5d3efa7f281011d2986
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-06 00:10:31 +0100

    Check valid password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -7,12 +7,14 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
+    """Encrypting passwords"""
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
 
 def is_valid(hashed_password: bytes, password: str):
+    """Check valid password"""
     try:
         hashed_password_str = hashed_password.decode('utf-8')
 
