b8e725c6520fb861f083833c448704bfc18048ca
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-08 01:13:29 +0100

    Error handler: Unauthorized

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -6,11 +6,20 @@ from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats/', strict_slashes=False)
-def stats():
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
     from models.user import User
     stats = {}
     stats['users'] = User.count()
@@ -18,4 +27,5 @@ def stats():
 
 @app_views.route("/unauthorized", methods=["GET"])
 def unauthorized_endpoint():
+    """Error handler: Unauthorized"""
     abort(401)
