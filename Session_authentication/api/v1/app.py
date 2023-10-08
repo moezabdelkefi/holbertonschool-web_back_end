909fae08b92cf29b4546bca8c5ba896bda5ecb2b
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-08 22:44:53 +0100

    Empty session

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -19,6 +20,8 @@ auth_type = getenv("AUTH_TYPE")
 if auth_type:
     if auth_type == "basic_auth":
         auth = BasicAuth()
+    elif auth_type == "session_auth":
+        auth = SessionAuth()
     else:
         from api.v1.auth.auth import Auth
         auth = Auth()
---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,7 @@
+# api/v1/auth/session_auth.py
+
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """class sessionauth"""
+    pass
