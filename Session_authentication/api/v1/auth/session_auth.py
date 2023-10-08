a04706f298e141b3dc409c6967f2ba9d76533064
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-08 22:48:21 +0100

    Create a session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 """create a emty class SessionAuth"""
 from api.v1.auth.auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """class sessionauth"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a Session ID for a user_id.
+
+        Args:
+            user_id (str): The user ID.
+
+        Returns:
+            str: The generated Session ID or None.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
+
