5e8e5ca74263bc4cacfa8a92af67cd3eba47a374
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-14 16:50:34 +0100

    Register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -22,7 +22,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email, password):
+    def register_user(self, email: str, password: str) -> User:
         try:
             existing_user = self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
