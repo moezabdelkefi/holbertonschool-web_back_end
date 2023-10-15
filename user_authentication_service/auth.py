69b1d3a1e29d840bd73485a0061dc60f3f8fda6a
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-15 14:15:28 +0100

    Destroy session

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -70,3 +70,8 @@ class Auth:
                 return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Remove the session from the database"""
+        user = self._db.find_user_by(id=user_id)
+        self._db.update_user(user.id, session_id=None)
