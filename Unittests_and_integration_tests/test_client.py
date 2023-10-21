fcc57928b884889050ea9043ab1382531852223a
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-22 00:47:44 +0100

    Mocking a property

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -4,6 +4,7 @@ from parameterized import parameterized
 from unittest.mock import patch
 from client import GithubOrgClient
 
+
 class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand([
         ('google', 'Google'),
@@ -14,8 +15,23 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         org_info = client.org()
 
-        mock_get_json.assert_called_once_with(f'https://api.github.com/orgs/{org_name}')
+        mock_get_json.assert_called_once_with(
+            f'https://api.github.com/orgs/{org_name}')
         self.assertEqual(org_info, {"name": expected_name})
+
+    def test_public_repos_url(self):
+        org_payload = {
+            "repos_url": "https://api.github.com/orgs/exampleorg/repos"
+        }
+
+        with patch('client.GithubOrgClient.org', return_value=org_payload):
+            client = GithubOrgClient("exampleorg")
+
+            public_repos_url = client._public_repos_url
+
+            self.assertEqual(public_repos_url,
+                             "https://api.github.com/orgs/exampleorg/repos")
+
 
 if __name__ == '__main__':
     unittest.main()
