9337dc2a6625f665da70ab65daad1a70e5d19f2d
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-22 20:21:31 +0100

    Parametrize templates

---
+++ i18n/3-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Parametrize templates"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _, gettext
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
