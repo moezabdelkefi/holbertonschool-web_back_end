838e704704c1d58eaca4a5544a693ba2e350eb84
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-22 01:09:34 +0100

    Basic Flask app

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
 from flask import Flask, render_template
+from os import getenv
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
