2b8a6381461ddcec248c164b748e7f0194765f95
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-30 11:50:49 +0100

    Storing lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -9,10 +9,24 @@ from functools import wraps
 def count_calls(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        key = method.__qualname__
+        key = method.__name__
         count_key = f"{key}_count"
         self._redis.incr(count_key)
         return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwds)
+        self._redis.rpush(outputs, str(data))
+        return data
     return wrapper
 
 
@@ -22,6 +36,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         if isinstance(data, (str, bytes)):
