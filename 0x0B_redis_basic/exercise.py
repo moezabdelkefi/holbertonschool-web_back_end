77e5f52184b53c7864ed4f11899fac2667d58939
moezabdelkef <moezbadelkefi17@gmail.com>
2023-10-30 11:42:02 +0100

    Incrementing values

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,6 +3,17 @@
 import redis
 import uuid
 from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        count_key = f"{key}_count"
+        self._redis.incr(count_key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -10,15 +21,13 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
-
         if isinstance(data, (str, bytes)):
             self._redis.set(key, data)
-
         elif isinstance(data, (int, float)):
             self._redis.set(key, str(data))
-
         return key
 
     def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
