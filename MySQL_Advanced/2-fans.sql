02112a3732455e730f408de29c83783c9f058087
moezabdelkef <moezbadelkefi17@gmail.com>
2023-11-03 16:23:22 +0100

    Best band ever!

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Calculate the number of fans for each country origin and rank them
-SELECT origin, SUM(fans) AS total_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY total_fans DESC;
+ORDER BY nb_fans DESC;
