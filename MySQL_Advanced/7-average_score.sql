c6aebfd64fcc26ca08b718d51465790125d73f8a
moezabdelkef <moezbadelkefi17@gmail.com>
2023-11-05 13:15:05 +0100

    Average score

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,23 +1,12 @@
--- Average score
-DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+-- 7. Average score
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT)
 BEGIN
-  DECLARE total_score FLOAT;
-  DECLARE total_count INT;
-
-  SELECT SUM(score) INTO total_score
-  FROM corrections
-  WHERE user_id = user_id;
-
-  SELECT COUNT(*) INTO total_count
-  FROM corrections
-  WHERE user_id = user_id;
-
-  IF total_count > 0 THEN
-    UPDATE users
-    SET average_score = total_score / total_count
-    WHERE id = user_id;
-  END IF;
-END;
-//
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+$$
 DELIMITER ;
