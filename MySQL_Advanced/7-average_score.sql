362c7c90eb40127c6b5507c8d607001cdfdd3919
moezabdelkef <moezbadelkefi17@gmail.com>
2023-11-05 13:13:13 +0100

    Average score

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,22 @@
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+  DECLARE total_score FLOAT;
+  DECLARE total_count INT;
+
+  SELECT SUM(score) INTO total_score
+  FROM corrections
+  WHERE user_id = user_id;
+
+  SELECT COUNT(*) INTO total_count
+  FROM corrections
+  WHERE user_id = user_id;
+
+  IF total_count > 0 THEN
+    UPDATE users
+    SET average_score = total_score / total_count
+    WHERE id = user_id;
+  END IF;
+END;
+//
+DELIMITER ;
