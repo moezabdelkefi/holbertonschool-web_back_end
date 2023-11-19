d8c7f711b8c4887d49122ec2f818fd5a8ae3bf19
moezabdelkef <moezbadelkefi17@gmail.com>
2023-11-19 22:24:08 +0100

    Primitive - Holberton Class

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,16 +1,16 @@
 class HolbertonClass {
-    constructor(size, location) {
-        this._size = size;
-        this._location = location;
-    }
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
 
-    valueOf() {
-        return this._size;
-    }
+  valueOf() {
+    return this._size;
+  }
 
-    toString() {
-        return this._location;
-    }
+  toString() {
+    return this._location;
+  }
 }
 
 export default HolbertonClass;
