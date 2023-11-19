247562c8032faa3443701aac38a1150b54d86969
moezabdelkef <moezbadelkefi17@gmail.com>
2023-11-19 22:11:40 +0100

    A Building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,4 +1,4 @@
-class Building {
+export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
   }
@@ -7,9 +7,13 @@ class Building {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
+  set sqft(value) {
+    if (typeof value === 'number') {
+      this._sqft = value;
+    }
+  }
+
+  static evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
-
-export default Building;
