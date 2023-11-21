c3c4404148a8d4343e3e691ef860a09878f6c198
moezabdelkef <moezbadelkefi17@gmail.com>
2023-11-21 16:23:10 +0100

    Basic list of objects

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,6 @@
-function getListStudents() {
-  return [
-    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstname: 'James', location: 'Columbia' },
-    { id: 3, firstname: 'Serena', location: 'San Francisco' },
-  ];
-}
+const getListStudents = () => [
+  { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstname: 'James', location: 'Columbia' },
+  { id: 3, firstname: 'Serena', location: 'San Francisco' },
+];
 export default getListStudents;
