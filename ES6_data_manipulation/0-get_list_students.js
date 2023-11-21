fca4639e561bec5e9a56e4ebe15bcb552fda961e
moezabdelkef <moezbadelkefi17@gmail.com>
2023-11-21 16:14:54 +0100

    Basic list of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+function getListStudents() {
+  return [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 3, firstname: 'Serena', location: 'San Francisco' },
+  ];
+}
+export default getListStudents;
