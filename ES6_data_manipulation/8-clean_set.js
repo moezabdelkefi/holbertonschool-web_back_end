d505fd5d4e5c265fccd6ded31b0e4d2dfcf731b2
moezabdelkef <moezbadelkefi17@gmail.com>
2023-11-22 16:46:52 +0100

    Clean set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, startString) {
+  const filteredValues = [...set].filter((value) => value.startsWith(startString));
+
+  const cleanedString = filteredValues.map((value) => value.slice(startString.length)).join('-');
+
+  return cleanedString;
+}
