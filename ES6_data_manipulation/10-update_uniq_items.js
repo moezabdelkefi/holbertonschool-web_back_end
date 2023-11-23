0f778d94eef1868c5c60f1835d4fb2fe57ff5299
moezabdelkef <moezbadelkefi17@gmail.com>
2023-11-23 10:25:17 +0100

    More map data structure

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,19 @@
+function updateUniqueItems(groceriesMap) {
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  const updatedMap = new Map();
+
+  groceriesMap.forEach((quantity, item) => {
+    if (quantity === 1) {
+      updatedMap.set(item, 100);
+    } else {
+      updatedMap.set(item, quantity);
+    }
+  });
+
+  return updatedMap;
+}
+
+export default updateUniqueItems;
