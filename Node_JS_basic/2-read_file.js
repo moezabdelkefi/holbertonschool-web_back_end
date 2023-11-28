a55467a89f1ee140f8e887d5404df95e4603acf9
moezabdelkef <moezbadelkefi17@gmail.com>
2023-11-28 15:07:06 +0100

    Reading a file synchronously with Node JS

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,13 +3,31 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8').split('\n').filter(Boolean);
-    const headers = data.shift().split(',').map((header) => header.trim());
+    const headers = data[0].split(',').map((header) => header.trim());
+    const studentsData = data.slice(1).map((line) => line.split(',').map((field) => field.trim()));
 
-    console.log(`Number of students: ${data.length}`);
+    const fieldIndex = headers.indexOf('field');
+    const firstNameIndex = headers.indexOf('firstname');
 
-    headers.forEach((field, index) => {
-      const fieldStudents = data.map((line) => line.split(',')[index]).filter(Boolean);
-      console.log(`Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudents.join(', ')}`);
+    const studentsByField = studentsData.reduce((acc, student) => {
+      const fieldName = student[fieldIndex];
+      const firstName = student[firstNameIndex];
+
+      if (fieldName && firstName) {
+        if (!acc[fieldName]) {
+          acc[fieldName] = { count: 0, students: [] };
+        }
+        acc[fieldName].count++;
+        acc[fieldName].students.push(firstName);
+      }
+      return acc;
+    }, {});
+
+    console.log(`Number of students: ${studentsData.length}`);
+
+    Object.keys(studentsByField).forEach((field) => {
+      const { count, students } = studentsByField[field];
+      console.log(`Number of students in ${field}: ${count}. List: ${students.join(', ')}`);
     });
   } catch (err) {
     throw new Error('Cannot load the database');
