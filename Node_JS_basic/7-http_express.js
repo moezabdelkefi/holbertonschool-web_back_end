c69c50836399d3b418f4abaccdfe761e514abc49
moezabdelkef <moezbadelkefi17@gmail.com>
2023-11-30 14:57:29 +0100

    Create a more complex HTTP server using Express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,56 +1,63 @@
 const express = require('express');
-const fs = require('fs').promises;
+const fs = require('fs');
 
 const app = express();
+const port = 1245;
 
-const getData = async (database) => {
-  try {
-    const data = await fs.readFile(database, 'utf8');
-    const students = data.split('\n').filter((line) => line);
+const parseCSV = (data) => {
+  const lines = data.split('\n').filter((line) => line.trim() !== '');
+  const students = lines.slice(1);
 
-    if (students.length) {
-      students.shift();
+  const studentsData = {};
+  students.forEach((student) => {
+    // eslint-disable-next-line no-unused-vars
+    const [firstName, lastName, age, field] = student.split(',');
+    if (!studentsData[field]) {
+      studentsData[field] = [];
     }
+    studentsData[field].push(`${firstName}`);
+  });
 
-    const csStudents = students.filter((line) => line.includes('CS'));
-    const sweStudents = students.filter((line) => line.includes('SWE'));
-    return {
-      students,
-      csStudents,
-      sweStudents,
-    };
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+  return studentsData;
 };
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.type('text').send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  const database = process.argv[2];
-  if (!database) {
-    res.status(500).send('Database not provided');
-    return;
-  }
+app.get('/students', (req, res) => {
+  try {
+    const databasePath = './database.csv';
+    const data = fs.readFileSync(databasePath, 'utf-8');
 
-  const data = await getData(database);
-  if (data) {
-    res.send(
-      `This is the list of our students\nNumber of students: ${data.students.length}\n`
-      + `Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`
-      + `Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`,
-    );
-  } else {
-    res.status(500).send('Cannot read the database');
+    if (data.trim() === '') {
+      res.type('text').send('Empty database');
+      return;
+    }
+
+    const studentsData = parseCSV(data);
+
+    let response = 'This is the list of our students\n';
+    let totalStudents = 0;
+
+    for (const field in studentsData) {
+      if (Object.prototype.hasOwnProperty.call(studentsData, field)) {
+        const studentsInField = studentsData[field].length;
+        response += `Number of students in ${field}: ${studentsInField}. List: ${studentsData[field].join(', ')}\n`;
+        totalStudents += studentsInField;
+      }
+    }
+
+    response += `Number of students: ${totalStudents}\n`;
+
+    res.type('text').send(response);
+  } catch (error) {
+    res.type('text').send('Error reading database');
   }
 });
 
-const port = 1245;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log('...');
 });
 
 module.exports = app;
