e464c23fa8a273acc9550f9db07745e0b6b1d131
moezabdelkef <moezbadelkefi17@gmail.com>
2023-12-01 15:42:34 +0100

    Combining descriptions

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -3,33 +3,25 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', () => {
-    describe('SUM operation', () => {
-        it('should return the rounded sum of two numbers', () => {
-            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-        });
+  describe('type=SUM', () => {
+    it('should return the rounded sum of two numbers', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+
+  describe('type=SUBTRACT', () => {
+    it('should return the rounded subtraction of two numbers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+  });
+
+  describe('type=DIVIDE', () => {
+    it('should return the rounded division of two numbers', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
     });
 
-    describe('SUBTRACT operation', () => {
-        it('should return the rounded subtraction of two numbers', () => {
-            assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-        });
+    it('should return "Error" when dividing by zero', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
-
-    describe('DIVIDE operation', () => {
-        it('should return the rounded division of two numbers if the denominator is not zero', () => {
-            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-        });
-
-        it('should return "Error" when dividing by zero', () => {
-            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-        });
-    });
-
-    describe('Invalid operation', () => {
-        it('should throw an error for an invalid operation type', () => {
-            assert.throws(() => {
-                calculateNumber('INVALID', 1.4, 4.5);
-            }, Error);
-        });
-    });
+  });
 });
