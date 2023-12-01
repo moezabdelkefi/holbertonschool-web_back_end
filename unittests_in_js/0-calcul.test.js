b8ce81a5e2fa06e34f6ab9613472049c4b4579b0
moezabdelkef <moezbadelkefi17@gmail.com>
2023-12-01 16:42:17 +0100

    fixing issue

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,19 +1,24 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', () => {
-  it('should return the sum of rounded numbers for integers', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
+describe('0-calcul.js - calculateNumber', () => {
+  describe('First number rounded', () => {
+    it('should return the correct sum when the first number is rounded', () => {
+      assert.strictEqual(calculateNumber(1, 3), 4);
+    });
   });
 
-  it('should return the sum of rounded numbers for mixed integers and floats', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  describe('Second number rounded', () => {
+    it('should return the correct sum when the second number is rounded', () => {
+      assert.strictEqual(calculateNumber(1, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
   });
 
-  it('should return the sum of rounded numbers for floats', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  describe('Both numbers rounded', () => {
+    it('should return the correct sum when both numbers are rounded', () => {
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+      // Add more test cases if needed
+    });
   });
-
-  // Add more test cases for edge cases if needed
 });
